3b80885a91eccd793960eeae58c5811b11097443
Prayskey <prayskeyo@gmail.com>
2026-04-16 13:34:27 +0100

    Update community UI and add like button to code

---
+++ public/js/futo-communities.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.save-btn');
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('saved');
+
+      const icon = btn.querySelector('svg');
+
+      if (btn.classList.contains('saved')) {
+        icon.classList.remove('text-gray-600');
+        icon.classList.add('text-red-500', 'fill-red-500');
+      } else {
+        icon.classList.remove('text-red-500', 'fill-red-500');
+        icon.classList.add('text-gray-600');
+      }
+    });
+  });
+});
